2b02bb8608615e0a4ccd612c726f086a4362e685
DanBlyz <benjo123gonza@gmail.com>
2026-04-21 00:38:57 -0400

    Adiciones de configuracion

---
+++ lang/es/passwords.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espera antes de volver a intentar.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => 'No podemos encontrar un usuario con ese correo electrónico.',
+    'reset_password_subject' => 'Restablecer la contraseña',
+    'reset_password_greeting' => '¡Hola!',
+    'reset_password_line_1' => 'Recibes este correo porque recibimos una solicitud para restablecer la contraseña de tu cuenta.',
+    'reset_password_action' => 'Restablecer Contraseña',
+    'reset_password_line_2' => 'Este enlace para restablecer la contraseña expirará en :count minutos.',
+    'reset_password_line_3' => 'Si no solicitaste un restablecimiento de contraseña, no se requiere ninguna acción.',
+    'reset_password_salutation' => 'Saludos,',
+];
